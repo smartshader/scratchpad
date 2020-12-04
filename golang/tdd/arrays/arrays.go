0073bb56a8059a3ab3b572e088765fa76b1fa061
Imad Jerra <smartshader@gmail.com>
2020-12-03 22:27:40 -0500

    Add sums of arrays

--- golang/tdd/arrays/arrays.go
+++ golang/tdd/arrays/arrays.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) (sum int) {
 
 	return
 }
+
+func SumAll(numbersToSum ...[]int) (sum []int) {
+	for _, a := range numbersToSum {
+		sum = append(sum, Sum(a))
+	}
+
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sum []int) {
+	for _, a := range numbersToSum {
+		if len(a) == 0 {
+			sum = append(sum, 0)
+		} else {
+			sum = append(sum, Sum(a[1:]))
+		}
+	}
+
+	return
+}
--- golang/tdd/arrays/arrays_test.go
+++ golang/tdd/arrays/arrays_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
@@ -11,4 +14,46 @@ func TestSum(t *testing.T) {
 	if got != want {
 		t.Errorf("want: %d, got: %d, given %v", want, got, numbers)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	numbers1 := []int{1, 2, 3, 4, 5}
+	numbers2 := []int{50, 100}
+
+	want := []int{15, 150}
+	got := SumAll(numbers1, numbers2)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("want: %d, got: %d, given %v and %v", want, got, numbers1, numbers2)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int, numbersToSum ...[]int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want: %d, got: %d, given %v", want, got, numbersToSum)
+		}
+	}
+
+	t.Run("sum of tails of some slices", func(t *testing.T) {
+		numbers1 := []int{1, 2, 3, 4, 5}
+		numbers2 := []int{50, 100}
+
+		want := []int{14, 100}
+		got := SumAllTails(numbers1, numbers2)
+
+		checkSums(t, got, want, numbers1, numbers2)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		var numbers1 []int
+		numbers2 := []int{50, 100}
+
+		want := []int{0, 100}
+		got := SumAllTails(numbers1, numbers2)
+
+		checkSums(t, got, want, numbers1, numbers2)
+	})
 }
